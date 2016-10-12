58217fcad0dc467c98abd1fbd1dc42e257d3a510
eringrace <erin.grace.writer@gmail.com>
2016-10-12 08:41:41 -0700

    Functions work! But...
    Functions are working to verify that the input is a digit, as well as to convert the digit to an integer.
    
    However, most recent error: 
    1. Program asks how many days you'll stay
    2. Enter invalid (i.e., non-digit) input
    3. Program correctly identifies that the input is not acceptable and asks for new input
    4. Enter valid (i.e., digit) input
    5. Program incorrectly identifies the input as invalid and asks for new input
    6. Program repeats the incorrect identification for three rounds, then identifies the input as valid
    
    Not sure what's going on here; need to investigate!

--- japan-calculator.py
+++ japan-calculator.py
@@ -1,12 +1,34 @@
+####################### TRAVELERS #######################
+
+def travelers(people):
+	if people.isdigit():
+		people = int(people)
+		return people
+	else:
+		people = raw_input("\nSorry, I only understand numerals and decimals. So how many people are going?")
+		return travelers(people)
+
+####################### FLIGHT #######################
+
+
+####################### DAYS #######################
+def duration(days):
+	if days.isdigit():
+		days = int(days)
+		return days
+	else:
+		days = raw_input("\nSorry, I only understand numerals and decimals. So how many people are going?")
+		return duration(days)
+
 ####################### MEALS #######################
 # Breakfast subtracts one meal, since flights from the US don't arrive in the morning.
 # Dinners subtracts one meal, since flights to the US leave before dinner.
 
 def meals():
-    breakfast = (750 * days) - 750 
+    breakfast = (750 * duration(days)) - 750 
     breakfast = breakfast - 750
-    lunch = 1500 * days
-    dinner = 2000 * days
+    lunch = 1500 * duration(days)
+    dinner = 2000 * duration(days)
     dinner = dinner - 2000
     meal_cost = breakfast + lunch + dinner
     return meal_cost
@@ -16,19 +38,23 @@ def meals():
 
 def accomodation(acc_var):
 	if acc_var == "1":
-		return 8500 * days
+		return 8500 * duration(days)
 	elif acc_var == "2":
-		return 4500 * days
-	elif acc_var == "3": 
-		return 3000 * days
+		return 4500 * duration(days)
+	elif acc_var == "3":
+		return 3000 * duration(days)
 	else:
 		acc_var = raw_input("\nSorry, I wasn't clear. If you want to stay in a hotel, press 1; if you want to stay in a guest house, press 2; and if you want to stay in a hostel, press 3.")
 		return accomodation(acc_var)
 
 ####################### TRAVEL #######################
 
-
-days = int(raw_input("How many days do you want to stay in Japan?"))
+print "So you're going to Japan! Okay..."
+people = raw_input("\nHow many travelers are there?")
+print "\nGreat! That's %s travelers." % travelers(people)
+print "\nNext we'll look at the flight cost."
+days = raw_input("\nHow many days do you want to stay?")
+print "\nGreat! That's %s days." % duration(days)
 print "\nThat'll be about %s yen in meals..." % (meals())
 acc_var = raw_input("\nWhat kind of accomodations do you want?\n     If you want to stay in a hotel, press 1.\n     If you want to stay in a guest house, press 2.\n     If you want to stay in a hostel, press 3. \n")
 print "\nSo you'll spend about %s on accommodations..." % (accomodation(acc_var))
